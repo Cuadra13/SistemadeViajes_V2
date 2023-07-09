9de67102a9cef648a22752150ba3e58b481f2f61
Cuadra13 <nelsoncuadra844@gmail.com>
2023-07-09 12:58:15 -0600

    Initial commit

---
+++ SistemadeViajes_V2/SistemadeViajes_V2/SistemadeViajes_V2/Program.cs
@@ -0,0 +1,14 @@
+using SistemadeViajes_V2;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
+
+
+
+var app = builder.Build();
+
+startup.Configure(app, app.Environment);
+
+app.Run();
